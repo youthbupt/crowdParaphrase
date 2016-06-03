30c5068813edd2ed9569440be3680b96e8a0153c
youthbupt <3230391@gmail.com>
2016-06-03 16:43:50 +0800

    123

--- crowdParaphrase/urls.py
+++ crowdParaphrase/urls.py
@@ -6,11 +6,12 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'crowdParaphrase.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^buildDatabaseFromFile/', 'HITModel.views.InsertFromFile'),
+    url(r'^buildDatabaseFromFile/', HITModel.views.InsertFromFile),
     url(r'^labelPage/?$', getLabelPage),
     url(r'^$', getHomePage),
     url(r'^userLogout/?$', userLogout),
     url(r'^userLogin/?$', userLogin),
     url(r'^checkLogin/?$', checkLogin),
+    url(r'^/removeParaphrase?$', HITModel.views.cleanParaphraseDatabase),
     # url(r'^admin/', include(admin.site.urls)),
 )
